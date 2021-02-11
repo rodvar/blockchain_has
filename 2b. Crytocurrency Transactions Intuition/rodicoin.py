4dc4cf91f4b9ba2aad851b7fbe2c5a8a1e3a3846
Rodrigo Varela <rodvar@gmail.com>
2021-02-11 19:02:39 +1100

    blockchain length on response - get chain

--- 2b. Crytocurrency Transactions Intuition/rodicoin.py
+++ 2b. Crytocurrency Transactions Intuition/rodicoin.py
@@ -146,7 +146,7 @@ def mine_block():
 @app.route('/get_chain', methods = ['GET'])
 def get_chain():
     response = {'blockchain' : blockchain.chain,
-                'length' : len(blockchain.chain)}.items()
+                'length' : len(blockchain.chain)}
     return jsonify(response), 200
 
 @app.route('/chain_valid', methods = ['GET'])
